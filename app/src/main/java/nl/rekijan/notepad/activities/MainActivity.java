41b066beeb58090556d74101f2dbf2785040a740
Erik-Jan Krielen <ej-krielen@gmail.com>
2016-07-10 20:29:13 +0200

    Added drawer navigation

--- app/src/main/java/nl/rekijan/notepad/activities/MainActivity.java
+++ app/src/main/java/nl/rekijan/notepad/activities/MainActivity.java
@@ -5,6 +5,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
 
 import nl.rekijan.notepad.R;
 import nl.rekijan.notepad.data.DatabaseHelper;
@@ -13,6 +23,7 @@ import nl.rekijan.notepad.fragments.NoteLinedEditorFragment;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private Drawer result = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+
+        setupDrawer(savedInstanceState);
 
         if (savedInstanceState == null){
             NoteLinedEditorFragment  fragment = new NoteLinedEditorFragment();
@@ -39,5 +52,64 @@ public class MainActivity extends AppCompatActivity {
                 .addToBackStack(null)
                 .commit();
         getSupportActionBar().setTitle(title);
+    }
+
+    private void setupDrawer(Bundle savedInstanceState) {
+        //Now build the navigation drawer and pass the AccountHeader
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.title_home).withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
+                        new PrimaryDrawerItem().withName(R.string.title_settings).withIcon(FontAwesome.Icon.faw_list).withIdentifier(2)
+
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int i, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable){
+                            String name = ((Nameable)drawerItem).getName().getText(MainActivity.this);
+                            mToolbar.setTitle(name);
+                        }
+
+                        if (drawerItem != null){
+                            int selectedScreen = drawerItem.getIdentifier();
+                            switch (selectedScreen){
+                                case 1:
+                                    //do nothing
+                                    break;
+                                case 2:
+                                    //go to settings screen, yet to be added
+                                    //this will be your home work
+                                    Toast.makeText(MainActivity.this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                                    break;
+                            }
+                        }
+                        return false;
+                    }
+
+
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View view) {
+                        KeyboardUtil.hideKeyboard(MainActivity.this);
+
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View view) {
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View view, float v) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
     }
 }
