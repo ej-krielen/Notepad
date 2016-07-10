6a3f586fd520b8f1489052d4162bf9fc531edcd1
Erik-Jan Krielen <ej-krielen@gmail.com>
2016-07-10 20:18:28 +0200

    Added styling

--- app/src/main/java/nl/rekijan/notepad/activities/MainActivity.java
+++ app/src/main/java/nl/rekijan/notepad/activities/MainActivity.java
@@ -1,19 +1,43 @@
 package nl.rekijan.notepad.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import nl.rekijan.notepad.R;
 import nl.rekijan.notepad.data.DatabaseHelper;
+import nl.rekijan.notepad.fragments.NoteLinedEditorFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        if (savedInstanceState == null){
+            NoteLinedEditorFragment  fragment = new NoteLinedEditorFragment();
+            openFragment(fragment, "Note Editor");
+        }
+
         DatabaseHelper databaseHelper = new DatabaseHelper(this);
         databaseHelper.getWritableDatabase();
+    }
+
+    private void openFragment(final Fragment fragment, String title){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
+        getSupportActionBar().setTitle(title);
     }
 }
--- app/src/main/java/nl/rekijan/notepad/activities/NoteEditorActivity.java
+++ app/src/main/java/nl/rekijan/notepad/activities/NoteEditorActivity.java
@@ -1,15 +1,34 @@
 package nl.rekijan.notepad.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import nl.rekijan.notepad.R;
 
 public class NoteEditorActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note_editor_acitivy);
+        setContentView(R.layout.activity_note_editor);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void openFragment(final Fragment fragment, String title){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
+        getSupportActionBar().setTitle(title);
     }
 }
